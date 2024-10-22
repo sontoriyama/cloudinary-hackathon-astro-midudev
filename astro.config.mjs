8c5ab639323a420de075bfb09e4bb827455ae190
sontoriyama <sontoriyama@gmail.com>
2024-10-22 09:36:44 +0200

    aviam si ara vercel i sino altres opcions

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 import fulldev from 'fulldev-ui/integration'
 
-import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
